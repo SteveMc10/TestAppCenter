36fdbc8bae5fc8846b4abeee807d11db0f1a999b
McGuinness, Steve <Steve.McGuinness@citizensbank.com>
2020-09-22 11:34:26 -0400

    Initial Commit

---
+++ TestAppCenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppCenter
+//
+//  Created by McGuinness, Steve on 9/22/20.
+//  Copyright © 2020 McGuinness, Steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
